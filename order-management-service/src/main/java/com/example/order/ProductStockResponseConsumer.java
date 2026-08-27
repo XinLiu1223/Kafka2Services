4d828caa568daf4254c51cd14a4c6fe59c34b087
XinLiu1223 <XinLiu1223@gmail.com>
2026-08-27 16:18:33 -0400

    create sync http email service call in Kafka async product stock check response

--- order-management-service/src/main/java/com/example/order/ProductStockResponseConsumer.java
+++ order-management-service/src/main/java/com/example/order/ProductStockResponseConsumer.java
@@ -5,19 +5,25 @@ import com.example.order.service.StockResponseManager;
 import com.example.order.syncservice.OrderService;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestClient;
+import org.springframework.web.client.RestClientException;
 
 @Component
 public class ProductStockResponseConsumer {
     private final StockResponseManager responseManager;
     private final OrderService orderService;
+    private final RestClient restClient;
 
     public ProductStockResponseConsumer(
             StockResponseManager responseManager,
-            OrderService orderService
+            OrderService orderService,
+            RestClient restClient
     ) {
 
         this.responseManager = responseManager;
         this.orderService = orderService;
+        this.restClient = restClient;
     }
 
     @KafkaListener(
@@ -33,6 +39,40 @@ public class ProductStockResponseConsumer {
             orderService.markConfirmed(
                     response.orderId()
             );
+
+
+            try {
+                String result = restClient
+                        .get()
+                        .uri(
+                                "http://localhost:8080/email?order-id={orderId}",
+                                response.orderId()
+                        )
+                        .retrieve()
+                        .body(String.class);
+
+                System.out.println(
+                        "Email service response: " + result
+                );
+
+            } catch (HttpClientErrorException e) {
+                System.err.println(
+                        "Email service returned HTTP error: "
+                                + e.getStatusCode()
+                                + ", body: "
+                                + e.getResponseBodyAsString()
+                );
+            } catch (RestClientException e) {
+                System.err.println(
+                        "Failed to call Email Service: "
+                                + e.getMessage()
+                );
+            } catch (Exception e) {
+                System.err.println(
+                        "Unexpected error: "
+                                + e.getMessage()
+                );
+            }
 
         } else {
             System.out.println("Order " + response.orderId() + " cannot proceed.");

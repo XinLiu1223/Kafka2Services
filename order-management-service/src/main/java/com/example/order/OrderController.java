f116f53b8151fb2028d343f255e7b27b916bf457
XinLiu1223 <XinLiu1223@gmail.com>
2026-09-03 16:14:17 -0400

    add allow cross origin for react app local port

--- order-management-service/src/main/java/com/example/order/OrderController.java
+++ order-management-service/src/main/java/com/example/order/OrderController.java
@@ -7,15 +7,13 @@ import com.example.order.syncservice.OrderResponse;
 import com.example.order.syncservice.OrderService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.TimeUnit;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:5173")
 @RequestMapping("/orders")
 public class OrderController {
 
